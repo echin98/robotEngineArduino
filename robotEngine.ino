38d9e7dbad3f1bc8898aa21c5387f52a2f909098
echin98 <ericchin1998@gmail.com>
2019-03-19 20:56:42 -0400

    fully functional, may be beneficial to adjust slew limit a bit lower

--- robotEngine.ino
+++ robotEngine.ino
@@ -6,6 +6,8 @@
 #define RIGHT_OUT_PIN 5
 #define WEAPON_OUT_PIN 6
 
+#define SLEW_LIMIT 10.0
+
 void updateTurn();
 void updateWeapon();
 void updateThrottle();
@@ -31,11 +33,14 @@ void setup(){
 	Serial.begin(115200);
 }
 
-int leftTemp;
-int rightTemp;
+int leftTemp=1500;
+int rightTemp=1500;
+int weaponTemp=900;
 unsigned long lastT = 0l;
 
 void updateVals(){
+  int rightTempOld = rightTemp;
+  int leftTempOld = leftTemp;
   rightTemp = (curThrottle+curTurn);
   leftTemp = -(curThrottle-curTurn);
   
@@ -49,52 +54,54 @@ void updateVals(){
     rightTemp =(int)(rightTemp*(400.0l/ (abs(leftTemp)>abs(rightTemp)?abs(leftTemp):abs(rightTemp))));
   }
 
-  leftTemp += 1400l;
-  rightTemp += 1400l;
+  weaponTemp = curWeapon<1400?curWeapon:1200;
+
+  leftTemp += 1500l;
+  rightTemp += 1500l;
+  int leftSlew = leftTempOld-leftTemp;
+  leftSlew = abs(leftSlew);
+  int rightSlew = rightTempOld-rightTemp;
+  rightSlew = abs(rightSlew);
+  
+  if((leftSlew>rightSlew?leftSlew:rightSlew)>SLEW_LIMIT){
+    Serial.print((leftSlew>rightSlew?leftSlew:rightSlew));
+    Serial.print("   ");
+    Serial.print(rightTempOld);
+    Serial.print(" slew limit exceeded ");
+    leftTemp = (int)(leftTempOld+(SLEW_LIMIT*(leftTemp-leftTempOld)/(leftSlew>rightSlew?leftSlew:rightSlew)));
+    rightTemp = (int)(rightTempOld+(SLEW_LIMIT*(rightTemp-rightTempOld)/(leftSlew>rightSlew?leftSlew:rightSlew)));
+    Serial.println((leftTemp-leftTempOld));
+  }
+  
 }
 
 void loop(){
-
-  leftTemp = 0l;
-  rightTemp = 0l;
   
-  int leftState = LOW;
-  int rightState = LOW;
-  int weaponState = LOW;
-  
-  updateVals();
 
   unsigned long curMicros = micros();
-  lastT = curMicros;
   long tstamp = curMicros-lastT;
 
   static int test = LOW;
   digitalWrite(10,test);
   test = test==LOW?HIGH:LOW;
   
-  while(tstamp<20000){
-    curMicros = micros();
-    tstamp = curMicros-lastT;
-    Serial.println(tstamp);
+  if(tstamp>=20000){
+    updateVals();
+    lastT = curMicros;
+    digitalWrite(LEFT_OUT_PIN,HIGH);
+    delayMicroseconds(leftTemp);
+    digitalWrite(LEFT_OUT_PIN, LOW);
+    
+    digitalWrite(RIGHT_OUT_PIN,HIGH);
+    delayMicroseconds(rightTemp);
+    digitalWrite(RIGHT_OUT_PIN, LOW);
+    
+    digitalWrite(WEAPON_OUT_PIN,HIGH);
+    delayMicroseconds(weaponTemp);
+    digitalWrite(WEAPON_OUT_PIN, LOW);
 
-    if(tstamp<leftTemp)
-      leftState = HIGH;
-    else
-      leftState = LOW;
-      
-    if(tstamp<rightTemp)
-      rightState = HIGH;
-    else
-      rightState = LOW;
     
-    if(tstamp<curWeapon)
-      weaponState = HIGH;
-    else
-      weaponState = LOW;
     
-    digitalWrite(LEFT_OUT_PIN, leftState);
-    digitalWrite(RIGHT_OUT_PIN, rightState);
-    digitalWrite(WEAPON_OUT_PIN, weaponState);
   }
   /*
   Serial.print(leftTemp);
@@ -111,7 +118,7 @@ void updateTurn(){
 		start = micros();
 	}
 	else{
-		curTurn = micros()-start-1448;
+		curTurn = micros()-start-1500;
 	}
 }
 		
@@ -121,7 +128,7 @@ void updateThrottle(){
 		start = micros();
 	}
 	else{
-		curThrottle = micros()-start-1480;
+		curThrottle = micros()-start-1500;
 	}
 }
 
